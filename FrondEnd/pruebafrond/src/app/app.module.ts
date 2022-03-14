f725292ed469f1c285426aaa856371aa1dbafea5
faespanag <faespanag@uqvirtual.edu.co>
2022-03-14 11:46:07 -0500

    Segundo commit

--- FrondEnd/pruebafrond/src/app/app.module.ts
+++ FrondEnd/pruebafrond/src/app/app.module.ts
@@ -1,4 +1,6 @@
+import {  HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +14,11 @@ import { InicioComponent } from './Components/inicio/inicio.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // import para utilizar el formulario 
+    ReactiveFormsModule,
+    // import para utilizar la Api
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
